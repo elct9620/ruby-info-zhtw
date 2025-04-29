d56a15e584aaea03edf457c8d9c8e32c8d7a2788
Aotokitsuruya <contact@aotoki.me>
2025-04-29 19:56:37 +0800

    feat: Add email parsing with postal-mime library

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,14 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import * as PostalMime from 'postal-mime';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		return new Response('Hello World!');
 	},
 	async email(message, env, ctx) {
-		// Noop
+		const parser = new PostalMime.default();
+		const rawEmail = new Response(message.raw);
+		const parsedEmail = await parser.parse(await rawEmail.arrayBuffer());
+
+		console.debug(`Parsed: ${parsedEmail.text}`);
 	},
 } satisfies ExportedHandler<Env>;
