a2079079beff3e188bdee6767c9804f6b3c9e984
Aotokitsuruya <contact@aotoki.me>
2025-04-29 20:40:54 +0800

    feat: Add Issue interface and initial issue parsing logic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import * as PostalMime from 'postal-mime';
 
+interface Issue {
+	id: number;
+	subject: string;
+	description: string;
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
 		return new Response('Hello World!');
@@ -8,7 +14,13 @@ export default {
 		const parser = new PostalMime.default();
 		const rawEmail = new Response(message.raw);
 		const parsedEmail = await parser.parse(await rawEmail.arrayBuffer());
+		const body = parsedEmail.text;
 
-		console.debug(`Parsed: ${parsedEmail.text}`);
+		// TODO: Parse the body to extract the issue number
+		const issueLink = undefined;
+		const issue = (await (await fetch(`${issueLink}.json?include=journals`)).json()) as Issue;
+
+		console.debug(`Subject: ${issue.subject}`);
+		console.debug(`Description: ${issue.description}`);
 	},
 } satisfies ExportedHandler<Env>;
